802969e56b16d1ab05cfe415118bb5521a91b7d1
ManfredH <manfredh@devmem.com>
2017-11-23 10:52:54 +0100

    PostConstruct attribute added

--- spring-consume-rest/src/main/java/org/devmem/springconsumerest/DemoClient.java
+++ spring-consume-rest/src/main/java/org/devmem/springconsumerest/DemoClient.java
@@ -1,9 +1,12 @@
 package org.devmem.springconsumerest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.PostConstruct;
 
 @Component
 public class DemoClient {
@@ -11,13 +14,19 @@ public class DemoClient {
     @Autowired
     private RestTemplate restTemplate;
 
+    private String componentName;
+
+    public DemoClient( @Value("DemoClient") String componentName) {
+        this.componentName = componentName;
+    }
+
+    @PostConstruct
     public void getDataFromService() {
 
         String url_template = "https://api.github.com/repos/spring-projects/spring-framework/commits";
 
         String result;
-        result = restTemplate.getForObject("https://api.github.com/repos/spring-projects/spring-framework/commits",
-                String.class);
+        result = restTemplate.getForObject(url_template, String.class);
         System.out.println(result);
 
     }
